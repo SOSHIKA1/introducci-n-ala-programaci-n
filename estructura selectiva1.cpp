0ee04b15f383c840421693c72aefe7b002ead7a8
Jesus Pari <pjmamani0@gmail.com>
2026-09-14 14:13:32 -0500

    agregando ejercicio

---
+++ estructura selectiva1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int N;
+cout<<"un numero?";
+cin >> N;
+if (N%2==0)
+{
+    cout <<"es par";
+}
+else 
+{
+    cout<<"es impar";
+}
+    return 0;
+}
